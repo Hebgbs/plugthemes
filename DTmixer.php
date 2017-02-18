234c2e4cdf41ee891a9e58b8cb2018b47f706cf8
Hebgbs <bluufoxx@gmail.com>
2017-02-18 18:35:18 -0500

    Happy birthday!
    Created for use with CMS; Still testing things.

---
+++ DTmixer.php
@@ -0,0 +1,10 @@
+<?php
+    header("Access-Control-Allow-Origin: *");
+?>
+{
+    "css": "https://hebgbs.github.io/DTmixer.css",
+//    "social": {
+//        "icon": "https://example.com/example.png",
+//        "link": "https://exmaple.com"
+//    }
+}
